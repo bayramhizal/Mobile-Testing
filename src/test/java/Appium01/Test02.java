7efb89eb2abdba4c7e7671b798cb82069e34d8d3
bayramhizal <bayramhizal41@icloud.com>
2022-09-07 09:07:35 +0200

    assert

--- src/test/java/Appium01/Test02.java
+++ src/test/java/Appium01/Test02.java
@@ -4,17 +4,17 @@ import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.junit.Test;
-import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class Test01 {
+public class Test02 {
 
     @Test
 
-    public void test01() throws MalformedURLException {
+    public void test02() throws MalformedURLException, InterruptedException {
         DesiredCapabilities desiredCapabilities=new DesiredCapabilities();
       // desiredCapabilities.setCapability( "platformName","Android" );key ve value olarak da yazilabilir
        // desiredCapabilities.setCapability( CapabilityType.PLATFORM_NAME,"Android" ); diye de yazilabilir
@@ -28,5 +28,14 @@ public class Test01 {
        // URL url=new URL("http://17.0.0.1:4723/wd/hub");
        // AndroidDriver<MobileElement>driver=new AndroidDriver<MobileElement>( url,desiredCapabilities );//webelelemnmt ya da android element de yazilabilir
         AndroidDriver<MobileElement> driver=new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"),desiredCapabilities );
+        driver.findElementById( "com.android.permissioncontroller:id/continue_button" ).click();
+        Thread.sleep( 3000 );
+        MobileElement okButton=driver.findElementById( "android:id/button1" );
+        Thread.sleep( 3000 );
+        okButton.click();
+        Thread.sleep( 3000 );
+        MobileElement homescrenTitle=driver.findElementById( "android:id/title" );
+        Thread.sleep( 3000 );
+        Assert.assertTrue( homescrenTitle.isDisplayed() );
     }
 }
