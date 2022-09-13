156effb9c7c7bd4307c00a5b88e3dcc8f9ee807c
bayramhizal <bayramhizal41@icloud.com>
2022-09-13 21:21:07 +0200

    flag

---
+++ src/test/java/Appium01/apiDemos02.java
@@ -0,0 +1,58 @@
+package Appium01;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class apiDemos02 {
+    @Test
+    public void test01() throws MalformedURLException {
+       /*
+       api demos intall edildi
+       ana sayfadayiz
+       apidemos tikla
+       preference tikla
+       preference dependencies tikla
+       wifi check box cehck yap
+       wifi settings tikla
+       text gonder
+       ok tikla
+        */
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability( MobileCapabilityType.PLATFORM_NAME, "Android" );
+        desiredCapabilities.setCapability( MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2" );
+        desiredCapabilities.setCapability( MobileCapabilityType.PLATFORM_VERSION, "10.0" );
+        desiredCapabilities.setCapability( MobileCapabilityType.DEVICE_NAME, "emulator-5554" );
+        desiredCapabilities.setCapability( MobileCapabilityType.APP, "/Users/bayram/IdeaProjects/ApiumKurulum/src/test/Apps/apiDemos.apk" );
+        desiredCapabilities.setCapability( "appPackage", "com.touchboarder.android.api.demos" );
+        desiredCapabilities.setCapability( "appActivity", "com.touchboarder.androidapidemos.MainActivity" );
+        desiredCapabilities.setCapability( "noReset", true );
+        AndroidDriver<MobileElement> driver=new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"),desiredCapabilities );
+        //apidemos tikla
+        driver.findElementByXPath( "//android.widget.TextView[@text='API Demos']" ).click();
+        //preference tikla
+        driver.findElementByXPath( "//android.widget.TextView[@text='Preference']" ).click();
+        //   preference dependencies tikla
+        driver.findElementByXPath( "//android.widget.TextView[@text='3. Preference dependencies']" ).click();
+        //    wifi check box cehck yap
+        MobileElement checkBox=driver.findElementById( "android:id/checkbox" );
+        boolean flag=true;
+        if (checkBox.isEnabled()) {
+            flag=false;
+        }
+
+        //wifi settings tikla
+        driver.findElementByXPath( "//android.widget.TextView[@text='WiFi settings']" ).click();
+        //text gonder
+        MobileElement sendTex=driver.findElementById( "android:id/edit" );
+        sendTex.sendKeys( "Der Versuch" );
+        MobileElement okButton=driver.findElementById( "android:id/button1" );
+        okButton.click();
+
+    }
+}
