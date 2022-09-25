76789b586ba1de8c8386a8611626f79c2a4c1936
bayramhizal <bayramhizal41@icloud.com>
2022-09-25 20:30:45 +0200

    uiselectorScroll

---
+++ src/test/java/Appium01/UiScroable.java
@@ -0,0 +1,36 @@
+package Appium01;
+
+import Appium.BaseClass;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.touch.offset.PointOption;
+import org.junit.Test;
+import org.openqa.selenium.Dimension;
+
+import java.net.MalformedURLException;
+
+public class UiScroable extends BaseClass {
+    @Test
+    public void test() throws MalformedURLException {
+        AndroidDriver driver=getAndroidDriver();
+        driver.findElementByXPath( "//android.widget.TextView[@text='API Demos']" ).click();
+        //preference tikla
+        driver.findElementByXPath( "//android.widget.TextView[@text='Views']" ).click();
+        //   preference dependencies tikla
+        //uiselector
+        driver.findElementByAndroidUIAutomator( "new UiScrollable(new UiSelector()).scrollIntoView(text(\"Switches\"))" );
+        driver.findElementByAndroidUIAutomator( "UiSelector().className(\"android.widget.TextView\").text(\"Switches\")" ).click();
+
+     /*   Dimension dimension=driver.manage().window().getSize();
+        int start_x=(int)(dimension.width*0.5);
+        int start_y=(int)(dimension.height*0.8);
+        int end_x=(int)(dimension.width*0.5);
+        int end_y=(int)(dimension.height*0.2);
+        for (int i=0;i<4;i++){
+            TouchAction touchAction=new TouchAction( driver );
+            touchAction.press( PointOption.point( start_x,start_y ) ).moveTo( PointOption.point( end_x,end_y ) ).release().perform();
+
+        }*/
+
+    }
+}
